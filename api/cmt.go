c091194cd1231117f1ea83d6441083237b79e80d
xulin <xl9211@163.com>
2022-11-10 14:02:02 +0800

    init

--- api/cmt.go
+++ api/cmt.go
@@ -135,7 +135,6 @@ func (s *CmtRPCService) GetAllAddress() (string, error) {
 	for it.Next() {
 		addr := tempTrie.GetKey(it.Key)
 		addresses = append(addresses, common.Bytes2Hex(addr))
-		fmt.Printf("VULCANLABS Address: {%s}\n", common.Bytes2Hex(addr))
 	}
 
 	data, _ := json.Marshal(addresses)
